b9756f0864c3e3799400055dbf7fe1d693025fcc
Guan Wei <phuaguanwei99@gmail.com>
2023-01-27 18:02:30 +0800

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,28 @@
-import express, { NextFunction, Request, Response, Router } from 'express';
-import employeeRouter from './routes/employees'
-import { json } from 'body-parser';
-import { Sequelize } from 'sequelize';
-import { sequelize } from '../sequelize';
+import express, { NextFunction, Request, Response, Router } from "express";
+import employeeRouter from "./routes/employees";
+import { json } from "body-parser";
+import { sequelize } from "../sequelize";
+
+var cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 app.use(json());
 
-app.use('/employee', employeeRouter);
+app.use("/employee", employeeRouter);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({message:err.message});
+  res.status(500).json({ message: err.message });
 });
 
 app.listen(3000, async () => {
   console.log("Application listening at http://localhost:3000");
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database: ', error);
+    console.error("Unable to connect to the database: ", error);
   }
 });
