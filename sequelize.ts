9f3b0660d0d53bc74cd6502207c43b52db39ae96
Guan Wei <phuaguanwei99@gmail.com>
2023-02-15 11:23:46 +0800

    npm install, and debugging alternative

--- sequelize.ts
+++ sequelize.ts
@@ -1,9 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 import { Sequelize } from "sequelize";
 import config from "./config/config";
 
 console.log(process.env.NODE_ENV);
 const dbConfig = (config as any)[process.env.NODE_ENV || ''];
 console.log(config);
-
 export const sequelize = new Sequelize(dbConfig);
+
+// export const sequelize = new Sequelize("employee", "postgres", "Phua0107", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
